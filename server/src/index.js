ce7c0935853be443ff3a1784a44327c6aeaa731a
SumitB1412 <beniwalsumit1412@gmail.com>
2022-12-27 23:07:25 +0530

    update index

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Homepage");
+  res.send("The landing page here");
 });
 
 
