0b6e6772d70ef408999c940f329af40ba72ae106
SumitB1412 <beniwalsumit1412@gmail.com>
2023-01-24 22:13:26 +0530

    update the nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+// import { Container } from "@chakra-ui/react";
+import styles from "../styles/navbar.module.css";
 
 export const Navbar = () => {
-  return <nav></nav>;
+  return <nav className={styles.navbar}>
+    
+  </nav>;
+  //   <Container h={"50px"} maxW={"100%"} border={"1px solid"}></Container>
 };
