058f8c18696d2e190f5bf15e77e28c4c0a7f512e
SumitB1412 <beniwalsumit1412@gmail.com>
2023-01-24 22:33:23 +0530

    update the nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 // import { Container } from "@chakra-ui/react";
 import styles from "../styles/navbar.module.css";
+import { Flex, Box,Spacer } from '@chakra-ui/react'
 
 export const Navbar = () => {
-  return <nav className={styles.navbar}>
-    
-  </nav>;
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.upperNav}>
+        <Flex fontSize={11} bgColor={"red"} w={300}  letterSpacing={1}>
+          <Box>Offers</Box>
+          <Spacer/>
+          <Box>Fanbook</Box>
+          <Spacer/>
+          <Box>Download App</Box>
+          <Spacer/>
+          <Box>TriBe Membership</Box>
+        </Flex>
+      </div>
+    </nav>
+  );
   //   <Container h={"50px"} maxW={"100%"} border={"1px solid"}></Container>
 };
